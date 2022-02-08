e3a714520134776480c50d70e3b4b70d8d943360
Nathan Igo <knowledgelord12@gmail.com>
2022-02-07 23:16:26 -0700

    Minor push for chapter 6.

---
+++ IJPDS/src/MathChr.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+public class MathChr {
+    public static void main(String[] args) {
+        Math.toRadians(Math.PI / 2);
+        Math.exp(2);
+        byte b = 'a';
+        int i = 'a';
+        System.out.println(i);
+        System.out.println("\\");
+    }
+}
